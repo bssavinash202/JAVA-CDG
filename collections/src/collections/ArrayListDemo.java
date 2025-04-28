696fac4a507dcf236e6d41d2bb2529349951effe
bssavinash202 <bssavinash202@gmail.com>
2025-04-28 10:36:25 +0530

    Set: HashSet, LinkedHashSet

--- collections/src/collections/ArrayListDemo.java
+++ collections/src/collections/ArrayListDemo.java
@@ -1,6 +1,7 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ArrayListDemo {
 	public static void main(String[] args) {
@@ -13,14 +14,23 @@ public class ArrayListDemo {
 //		for(String i:fruitsList) {
 //			System.out.print(i+" ");
 //		}
+//		System.out.println(fruitsList);
+//		System.out.println(fruitsList.contains("Apple"));// checks whether the specified object is present or not,return
+//															// Boolean
+//		System.out.println(fruitsList.get(0));// returns an object present in specified index
+//		System.out.println(fruitsList.remove(3));// removes the object at specified index
+//		System.out.println(fruitsList.indexOf("Apple"));// returns index of a specified object in ArrayList
+		Collections.sort(fruitsList);
 		System.out.println(fruitsList);
-		System.out.println(fruitsList.contains("Apple"));// checks whether the specified object is present or not,return
-															// Boolean
-		System.out.println(fruitsList.get(0));// returns an object present in specified index
-		System.out.println(fruitsList.remove(3));// removes the object at specified index
-		System.out.println(fruitsList.indexOf("Apple"));// returns index of a specified object in ArrayList
-		System.out.println(fruitsList);
-
+		
+		ArrayList<Integer> numList = new ArrayList<>();
+		numList.add(100);
+		numList.add(0);
+		numList.add(4090);
+		numList.add(190);
+		Collections.sort(numList);
+		System.out.println(numList);
+		
 	}
 
 }
