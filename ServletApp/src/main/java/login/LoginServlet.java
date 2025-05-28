d19904a6ef3a78bce9b72dcbddecc0a94d36f02a
bssavinash202 <bssavinash202@gmail.com>
2025-05-28 10:41:04 +0530

    Servlets

--- ServletApp/src/main/java/login/LoginServlet.java
+++ ServletApp/src/main/java/login/LoginServlet.java
@@ -3,8 +3,9 @@ package login;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,30 +13,50 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-//@WebServlet("/LoginServlet")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		String username = request.getParameter("username");
-		String password = request.getParameter("password");
+	
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
 		
-		if("admin".equals(username) && "Avinash@202".equals(password)) {
-			out.println("<h2> Welcome, "+username+"</h2>");
-			ServletConfig config = getServletConfig();
-			String adminEmail = config.getInitParameter("adminEmail");
-			out.println("please contact admin to resolve any issues: "+adminEmail);
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		String username = request.getParameter("username");
+		
+		String pass = request.getParameter("password");
+		
+		//store username in request attribute
+		request.setAttribute("name",username);
+		
+		out.println("<html><body>");
+		if("admin".equals(username) && "admin123".equals(pass)) {
+			// Redirecting the user to success page
+			//response.sendRedirect("Success.html");
+			//RequestDispatcher rd = request.getRequestDispatcher("welcome");
+			//rd.forward(request, response);
+			response.sendRedirect("welcome");
 			
-			String dev = config.getInitParameter("developer");
-			out.println("Servlet is developed by: "+dev);
-			
-			ServletContext context = getServletContext();
-			String con = context.getInitParameter("appName");
-			out.println("Application name"+ con);
-		}else {
-			out.println("<h2>Invalid usernmae or password, Please try again</h2>");
+			/*
+			 * ServletConfig config = getServletConfig(); String adminEmail =
+			 * config.getInitParameter("adminEmail");
+			 * out.println("<h3>Please contact admin to resolve any issues:"+adminEmail+
+			 * "</h3>");
+			 * 
+			 * String developer = config.getInitParameter("developer");
+			 * out.println("<h3>Servlet is developed by: "+developer+"</h3>");
+			 * 
+			 * ServletContext context = getServletContext(); String appName =
+			 * context.getInitParameter("appName");
+			 * out.println("<h3>Applecation: "+appName+"</h3>");
+			 */
 		}
-		out.close();
+		else {
+			RequestDispatcher rd = request.getRequestDispatcher("Error.html");
+			rd.forward(request, response);
+			//response.sendRedirect("Error.html");
+		}
+		out.println("</html></body>");
 	}
+	
 }
